04d679bdfe1ad6e4150a9a8d0a3ba8a409f40d91
xalys_ura <xalysjyra1992@gmail.com>
2025-05-15 19:55:43 +0300

    update time

--- tracker1.py
+++ tracker1.py
@@ -1,10 +1,12 @@
 import os
 import time
 import csv
+import pytz
 from datetime import datetime
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
 from telethon.tl.types import UserStatusOnline, UserStatusOffline
+from datetime import datetime
 
 load_dotenv(".env1")  
 
@@ -12,15 +14,16 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 username = os.getenv("USERNAME")
 
-today = datetime.now().strftime("%Y-%m-%d")
-log_file = f"log_{username.strip('@')}_{today}.csv"
+today = datetime.now(pytz.timezone("Europe/Kyiv")).strftime("%Y-%m-%d")
+log_file = f"logs/log_{username.strip('@')}_{today}.csv"
 
 if not os.path.exists(log_file):
     with open(log_file, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["timestamp", "status"])
 
-with TelegramClient("session_" + username.strip('@'), api_id, api_hash) as client:
+session_file = f"sessions/session_{username.strip('@')}"
+with TelegramClient(session_file, api_id, api_hash) as client:
     print(f"✅ Вхід успішний як {username}")
     print("▶️ Старт відстеження онлайн-статусу...")
 
@@ -29,11 +32,12 @@ with TelegramClient("session_" + username.strip('@'), api_id, api_hash) as clien
         try:
             user = client.get_entity(username)
             status = user.status
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            kyiv_tz = pytz.timezone("Europe/Kyiv")
+            now = datetime.now(kyiv_tz).strftime("%Y-%m-%d %H:%M:%S")
 
             if isinstance(status, UserStatusOnline):
                 current = "online"
-                interval = 3
+                interval = 8
             elif isinstance(status, UserStatusOffline):
                 current = "offline"
                 interval = 30
